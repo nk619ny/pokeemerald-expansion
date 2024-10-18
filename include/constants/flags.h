efcb36aba1f7037a80aa748dc6e441fc4d955dc1
nk619ny <nk619ny@yahoo.com>
2024-10-18 15:32:56 -0400

    1-10.18.24 First Test
    Testing Git for Version Control

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -652,9 +652,9 @@
 #define FLAG_HIDDEN_ITEM_ROUTE_123_RARE_CANDY                (FLAG_HIDDEN_ITEMS_START + 0x6E)
 #define FLAG_HIDDEN_ITEM_ROUTE_105_BIG_PEARL                 (FLAG_HIDDEN_ITEMS_START + 0x6F)
 
-#define FLAG_UNUSED_0x264  0x264 // Unused Flag
-#define FLAG_UNUSED_0x265  0x265 // Unused Flag
-#define FLAG_UNUSED_0x266  0x266 // Unused Flag
+#define FLAG_GRASS_STARTER_REMOVED  0x264 // NK Flag for starter selection
+#define FLAG_FIRE_STARTER_REMOVED  0x265 // Unused Flag
+#define FLAG_WATER_STARTER_REMOVED  0x266 // Unused Flag
 #define FLAG_UNUSED_0x267  0x267 // Unused Flag
 #define FLAG_UNUSED_0x268  0x268 // Unused Flag
 #define FLAG_UNUSED_0x269  0x269 // Unused Flag
