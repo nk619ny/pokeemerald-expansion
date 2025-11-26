2706af8e42becbb9ef8494b0a68dd734b8b5b5fb
nk619ny <nk619ny@yahoo.com>
2025-11-26 17:35:39 -0500

    13.3 11.26.25 Rewored TMs, save state, restarted

--- include/constants/global.h
+++ include/constants/global.h
@@ -80,7 +80,7 @@
 #define BAG_ITEMS_COUNT 30
 #define BAG_KEYITEMS_COUNT 30
 #define BAG_POKEBALLS_COUNT 16
-#define BAG_TMHM_COUNT 64
+#define BAG_TMHM_COUNT 158
 #define BAG_BERRIES_COUNT 46
 
 // Number of facilities for Ranking Hall.
