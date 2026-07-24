8090c3f308ff214ef82260fa53277fa6e249f553
nk619ny <nk619ny@yahoo.com>
2026-07-23 21:58:57 -0400

    Update flags.h

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1403,8 +1403,8 @@
 #define FLAG_SYS_SAFARI_MODE                        (SYSTEM_FLAGS + 0x2C)
 #define FLAG_SYS_CRUISE_MODE                        (SYSTEM_FLAGS + 0x2D)
 
-#define FLAG_UNUSED_0x88E                           (SYSTEM_FLAGS + 0x2E) // Unused Flag
-#define FLAG_UNUSED_0x88F                           (SYSTEM_FLAGS + 0x2F) // Unused Flag
+#define FLAG_ENABLE_BRINEY_BOAT                     (SYSTEM_FLAGS + 0x2E) // formerly not used
+#define FLAG_IN_BRINEY_BOAT                         (SYSTEM_FLAGS + 0x2F) // formerly not used
 
 #define FLAG_SYS_TV_HOME                            (SYSTEM_FLAGS + 0x30)
 #define FLAG_SYS_TV_WATCH                           (SYSTEM_FLAGS + 0x31)
