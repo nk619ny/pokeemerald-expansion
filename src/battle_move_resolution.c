a87c4f5ce12f9bf64432ae5fe5d006ad10b21bcd
nk619ny <nk619ny@yahoo.com>
2026-09-05 13:58:08 -0400

    Update battle_move_resolution.c

--- src/battle_move_resolution.c
+++ src/battle_move_resolution.c
@@ -4091,6 +4091,14 @@ static enum MoveEndResult MoveEndThirdMoveBlock(struct BattleCalcValues *cv)
             result = MOVEEND_RESULT_RUN_SCRIPT;
         }
         break;
+    case EFFECT_GEAR_UP:
+    // Custom: remove Electric Terrain after applying stat boosts
+        if (gFieldStatuses & STATUS_FIELD_ELECTRIC_TERRAIN)
+        {
+            BattleScriptCall(BattleScript_RemoveTerrain);
+            result = MOVEEND_RESULT_RUN_SCRIPT;
+        }
+        break;
     case EFFECT_STEEL_ROLLER:
         if (gFieldStatuses & STATUS_FIELD_TERRAIN_ANY && IsAnyTargetTurnDamaged(cv->battlerAtk, INCLUDING_SUBSTITUTES))
         {
